79dc7dc694f4957f70d8dcc1d0732aaa9ca1bae0
qorenx <icagatay930@gmail.com>
2022-04-10 23:09:40 +0300

    login page test

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,64 +1,34 @@
-<div style="margin-left:70px" class="w3-container">
-    <div class="row">
-        <div class="col-12 col-sm-8 offset-sm-2 col-md-4 offset-md-4 mt-5 pt-3 pb-3 bg-white form-wrapper">
-            <div class="container">
-                <h3>Login</h3>
-                <hr>
-                <?php $validation = \Config\Services::validation(); ?>
-                <?php if (session()->get('success')) : ?>
-                    <div class="alert alert-success" role="alert">
-                        <?= session()->get('success'); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if (session()->get('danger')) : ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?= session()->get('danger'); ?>
-                        <?php if (session()->get('resetlink')) {
-                            echo session()->get('resetlink');
-                        } ?>
-                    </div>
-                <?php endif; ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<div class="reglogBackground">
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-44 col-xl-5 reglogpanel">
 
-                <form class="" action="/login" method="post">
-                    <div class="form-group">
-                        <label for="email">Email Address</label>
-                        <input type="text" class="form-control" name="email" id="email" value="<?= set_value('email') ?>">
-                        <?php if ($validation->getError('email')) { ?>
-                            <div class='alert alert-danger mt-2'>
-                                <?= $error = $validation->getError('email'); ?>
-                            </div>
-                        <?php } ?>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" id="password" value="">
-                        <?php if ($validation->getError('password')) { ?>
-                            <div class='alert alert-danger mt-2'>
-                                <?= $error = $validation->getError('password'); ?>
-                            </div>
-                        <?php } ?>
-                    </div>
-                    <?php if ($config->rememberMe) : ?>
-                        <div class="form-check mb-3">
-                            <input type="checkbox" class="form-check-input" id="rememberme" name="rememberme" value="1">
-                            <label class="form-check-label" for="rememberme">Remember Me</label>
-                        </div>
-                    <?php endif; ?>
+          <div class="card-body p-5 text-center">
 
-                    <div class="row">
-                        <div class="col-12 col-sm-4">
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                        <div class="col-12 col-sm-8 text-right">
-                            <ul>
-                                <li><a href="/register">Sign Up</a></li>
-                                <li><a href="/forgotpassword">Forgot Password?</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </form>
+            <div class="mb-md-1 mt-md-1 pb-1">
+
+            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                <br></br/>
+              <div class="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" placeholder="Email" class="form-control form-control-lg" />
+
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" placeholder="Password" class="form-control form-control-lg" />
+
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
             </div>
+          </div>
 
-        </div>
+      </div>
     </div>
+  </div>
 </div>
+</section>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,7 +5,7 @@
 
     <title>Anitium</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" src="https://localhost/assets/css/custom.css">
+    <link rel="stylesheet" href="https://localhost/assets/css/custom.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
